15835034637b7df908165b31f1b690d92ae83fbe
원용완 <106060893+WonYongWan@users.noreply.github.com>
2024-04-25 11:26:38 +0900

    Delete test directory

--- test/js/header.js
+++
@@ -1,9 +0,0 @@
-const $header = document.querySelector('header');
-
-export function header(pageCount) {
-  if(pageCount === 2 || pageCount === 4) {
-    $header.classList.add('white');
-  } else {
-    $header.classList.remove('white');
-  }
-}
