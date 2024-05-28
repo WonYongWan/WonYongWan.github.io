5fffffbec20afb6111bd3c3e4159fae5e3282f20
원용완 <106060893+WonYongWan@users.noreply.github.com>
2024-05-28 17:17:36 +0900

    Delete test directory

--- test/js/footer.js
+++
@@ -1,6 +0,0 @@
-export function footer() {
-  const $footer = document.querySelector('footer');
-  const fooHeight = window.innerHeight - (window.innerHeight - $footer.clientHeight);
-  console.log(fooHeight)
-  $footer.style.height = `${fooHeight}px`;
-}
--- test/js/page1.js
+++
@@ -1,9 +0,0 @@
-import { specificMove } from './fullPage.js';
-
-const $btn = document.querySelectorAll('.page1 .btn_box a')[0];
-
-export function page1() {
-  $btn.addEventListener('click', () => {
-    specificMove($btn.dataset.fpnum);
-  });
-}
