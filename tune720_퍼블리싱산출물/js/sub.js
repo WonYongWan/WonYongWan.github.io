e720beb4804978dcb7a5f1d2018aaa47769312be
원용완 <106060893+WonYongWan@users.noreply.github.com>
2024-09-05 17:50:14 +0900

    Update sub.js

--- tune720_퍼블리싱산출물/js/sub.js
+++ tune720_퍼블리싱산출물/js/sub.js
@@ -5,13 +5,5 @@ const $subTabMenuTabs = document.querySelectorAll('.sub-3 .sub-tab-menu .tab');
 window.addEventListener('load', () => {
   $loadBox.classList.add('on');
 
-  if(window.innerWidth < 520) {
-    $subTabMenuTabs.forEach((elm, idx) => {
-      if(elm.classList.contains('on')) {
-        if(idx > $subTabMenuTabs.length - 3) {
-          $subTabMenuWrap.scrollLeft += Number(getComputedStyle($subTabMenuWrap).width.replace(/[^0-9]/g, "") - 20)
-        }
-      }
-    })
-  }
+  
 });
